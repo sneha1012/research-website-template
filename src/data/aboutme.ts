f57beb5704e2f93c1b544a16fe0169d5277d8610
Sneha Maurya <snehamaurya1012@gmail.com>
2025-01-17 11:02:02 -0500

    few changes

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,17 +23,16 @@ export const aboutMe: AboutMe = {
   institution: "Columbia University",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "I'm a first year <a href='https://www.engineering.columbia.edu'> Master's candidate</a> working at the intersection of NLP and HCI. My interests broadly span Machine learning, Systems, Natural Language Procesing, and Computer Vision. I focus on developing robust, interpretable systems that can leverage Large Distributed Models to help humans improve their efficiency.",
+  email: "sm5755@columbia.edu",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
+  githubUsername: "https://github.com/sneha1012",
+  linkedinUsername: "https://www.linkedin.com/in/snehamaurya10/",
+  bluesky: "https://bsky.app/profile/snehamaurya.bsky.social",
   blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  cvUrl: "https://drive.google.com/file/d/1ArFpet7W6KPQ73Px_HMNPvpaIWT-KRzp/view?usp=sharing",
+  institutionUrl: "https://www.engineering.columbia.edu",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
